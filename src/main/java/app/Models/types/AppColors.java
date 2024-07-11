307f1d80bf1655bd836fada27d21176b6b70a05a
raul espina <respina@bigwise.com>
2024-07-10 20:49:02 -0400

    feature: se creo la base del layout para la aplicacion, este contiene un sidebar y un contenerdor body

---
+++ src/main/java/app/Models/types/AppColors.java
@@ -0,0 +1,8 @@
+package app.Models.types;
+
+import java.awt.Color;
+
+public class AppColors {
+    public static final Color COLUMBIA_BLUE = new Color(210, 231, 255);
+    public static final Color LAPIS_LAZULI = new Color(16, 84, 129);
+}
